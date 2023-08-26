445f9d976265276faad345b6112db8b266019349
andrea <a.neira@pucp.edu.pe>
2023-08-25 20:29:58 -0500

    navbar

---
+++ src/test/java/com/example/lab1_gticss/Lab1GticssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1_gticss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1GticssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
